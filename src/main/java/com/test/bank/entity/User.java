2cfeca013ab8a2d62a3d94f0d27f0bd5497fdb74
Vladislav <dawvvlad@users.noreply.github.com>
2024-11-14 22:32:45 +0300

    package

---
+++ src/main/java/com/test/bank/entity/User.java
@@ -0,0 +1,13 @@
+package com.test.bank.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    private Long id;
+
+}
