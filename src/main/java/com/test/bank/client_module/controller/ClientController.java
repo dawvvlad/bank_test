7c348e358baa0b10e9534237b63933ea97a0bd44
Vladislav <dawvvlad@users.noreply.github.com>
2024-11-17 19:57:27 +0300

    controller

--- src/main/java/com/test/bank/client_module/controller/ClientController.java
+++ src/main/java/com/test/bank/client_module/controller/ClientController.java
@@ -2,12 +2,12 @@ package com.test.bank.client_module.controller;
 
 import com.test.bank.client_module.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class ClientController {
 
