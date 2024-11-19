829e4d432f1af64767589e2c4c53f0888d897d01
Vladislav <dawvvlad@users.noreply.github.com>
2024-11-19 22:56:20 +0300

    update

--- src/main/java/com/test/bank/credit_agreement_module/controller/CreditAgreementController.java
+++ src/main/java/com/test/bank/credit_agreement_module/controller/CreditAgreementController.java
@@ -1,9 +1,11 @@
 package com.test.bank.credit_agreement_module.controller;
 
+import com.test.bank.client_module.dto.ClientDTO;
 import com.test.bank.credit_agreement_module.dto.CreditAgreementDTO;
 import com.test.bank.credit_agreement_module.entity.CreditAgreement;
 import com.test.bank.credit_agreement_module.enums.AgreementStatus;
 import com.test.bank.credit_agreement_module.service.CreditAgreementService;
+import com.test.bank.credit_application_module.dto.CreditApplicationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,6 +23,19 @@ public class CreditAgreementController {
     @Autowired
     public CreditAgreementController(CreditAgreementService creditAgreementService) {
         this.creditAgreementService = creditAgreementService;
+    }
+
+    @GetMapping("/{id}")
+    public String getCreditAgreement(@PathVariable("id") Long id, Model model) {
+
+        CreditAgreementDTO creditAgreementDTO = creditAgreementService.findById(id);
+        CreditApplicationDTO creditApplicationDTO = creditAgreementDTO.getApplication();
+        ClientDTO clientDTO = creditApplicationDTO.getClient();
+
+        model.addAttribute("creditAgreement", creditAgreementDTO);
+        model.addAttribute("client", clientDTO);
+        model.addAttribute("creditApplication", creditApplicationDTO);
+        return "sign_agreement";
     }
 
     @PostMapping("/{id}")
